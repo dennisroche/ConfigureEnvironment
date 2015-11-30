034983958f44020a42fece9488e44c5519ad6777
Dennis Roche <droche@abngroup.com.au>
2015-11-30 13:22:46 +0800

    Made install script work in PS v4

--- Install.ps1
+++ Install.ps1
@@ -1,3 +1,4 @@
+#Requires -Version 4.0
 # Error handling
 $ErrorActionPreference = "Stop"
 
@@ -5,7 +6,6 @@ trap
 {
     Pop-Location
     $Host.UI.WriteErrorLine($_)
-    Exit 1
 }
 
 # Install-ModuleFromGitHub
@@ -17,23 +17,49 @@ function Install-ModuleFromGitHub {
         $Project
     )
 
+    function Expand-Zip {
+        param (
+            [Parameter(Position=0, Mandatory)]
+            [ValidateNotNullorEmpty()]
+            [ValidateScript({ Test-Path $_ })]
+            [ValidatePattern("\.zip$")]
+            [string]$Path,
+
+            [Parameter(Position=1, Mandatory)]
+            [ValidateNotNullorEmpty()]
+            [string]$DestinationPath
+        )
+
+        New-Item -ItemType Directory -Force -Path $DestinationPath | Out-Null
+        [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
+        [System.IO.Compression.ZipFile]::ExtractToDirectory((Resolve-Path $Path), (Resolve-Path $DestinationPath))
+    }
+
     try 
     {
     	# Note: you need to tag the release with `latest' for this to work
         $Url = "https://github.com/$GitHubUser/$Project/releases/download/latest/$Project.zip"
         $InstallPath = Join-Path -Path (Split-Path -Path $Profile) -ChildPath "\Modules\$Project"
 
-        if (!(Test-Path -Path $InstallPath)) {
+        if ((Test-Path -Path $InstallPath)) {
+            Remove-Item -Path $InstallPath -Recurse -Force | Out-Null
+        } else {
             New-Item -Path $InstallPath -ItemType Directory | Out-Null
         }
 
-        $DownloadPath = Join-Path -Path $InstallPath -ChildPath "$Project.zip"
+        $DownloadPath = "$env:temp\$Project.zip"
+        $ExtractPath = "$env:temp\$([guid]::NewGuid().ToString())"
+
         Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
-        Expand-Archive -Path $DownloadPath -DestinationPath $InstallPath
+        Expand-Zip -Path $DownloadPath -DestinationPath $ExtractPath
+        
+        Copy-Item $ExtractPath -Destination $InstallPath -Recurse
+        Write-Host -ForegroundColor Green "Successfully installed $Project to '$InstallPath'"
     }
     finally 
     {
-        Remove-Item -Path $DownloadPath
+        Remove-Item -Path $ExtractPath -Recurse -Force | Out-Null
+        Remove-Item -Path $DownloadPath | Out-Null
     }
 }
 
