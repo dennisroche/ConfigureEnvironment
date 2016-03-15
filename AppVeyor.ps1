e5fb22c0168abf760b7c6a7509eb4305f23bc12c
Dennis Roche <dennis.roche@readify.net>
2016-03-15 21:38:18 +0800

    Rev appveyor.ps1

--- AppVeyor.ps1
+++ AppVeyor.ps1
@@ -1,21 +1,24 @@
 [CmdletBinding()]
 param()
 
-$ModuleName = $env:ModuleName
-$ModuleLocation = $env:APPVEYOR_BUILD_FOLDER
-$PublishingNugetKey = $env:nugetKey
-$Psd1Path = "./$ModuleName.psd1"
-$BuildNumber = $env:APPVEYOR_BUILD_NUMBER
-
 $ErrorActionPreference = "Stop"
 trap {
     Write-Error $_
     Exit 1
 }
 
-# Use PSScriptAnalyzer to static analyse code https://github.com/powershell/psscriptanalyzer
-Import-Module -Name PSScriptAnalyzer
+$ModuleName = $env:ModuleName
+$PublishingNugetKey = $env:nugetKey
+$Psd1Path = "./$ModuleName.psd1"
+$BuildNumber = $env:APPVEYOR_BUILD_NUMBER
 
+# Add APPVEYOR_BUILD_FOLDER to modules path to enable discovery 
+$env:psmodulepath = $env:psmodulepath + ";" + $env:APPVEYOR_BUILD_FOLDER
+
+# Use PSScriptAnalyzer to static analyse PS1 https://github.com/powershell/psscriptanalyzer
+Import-Module -Name PSScriptAnalyzer -Force
+
+# Rules
 $rules = Get-ScriptAnalyzerRule -Severity Warning,Error
 $results = Invoke-ScriptAnalyzer -Path "C:\Program Files\WindowsPowerShell\Modules\$ModuleName" -IncludeRule $rules.RuleName -Recurse
 $results
