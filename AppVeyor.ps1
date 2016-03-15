835a84cf5a7dcb98f4ee903f9f304e6f7d3f3202
Dennis Roche <dennis.roche@readify.net>
2016-03-15 22:17:45 +0800

    Run analyser over current AppVeyor dir

--- AppVeyor.ps1
+++ AppVeyor.ps1
@@ -12,15 +12,13 @@ $PublishingNugetKey = $env:nugetKey
 $Psd1Path = "./$ModuleName.psd1"
 $BuildNumber = $env:APPVEYOR_BUILD_NUMBER
 
-# Add APPVEYOR_BUILD_FOLDER to modules path to enable discovery 
-$env:psmodulepath = $env:psmodulepath + ";" + $env:APPVEYOR_BUILD_FOLDER
-
 # Use PSScriptAnalyzer to static analyse PS1 https://github.com/powershell/psscriptanalyzer
-Import-Module -Name PSScriptAnalyzer -Force
+Import-Module -Name PSScriptAnalyzer -Force -Verbose
 
 # Rules
+Write-Output 'Invoke-ScriptAnalyzer -Path "$($env:APPVEYOR_BUILD_FOLDER)" -IncludeRule $rules.RuleName -Recurse'
 $rules = Get-ScriptAnalyzerRule -Severity Warning,Error
-$results = Invoke-ScriptAnalyzer -Path "C:\Program Files\WindowsPowerShell\Modules\$ModuleName" -IncludeRule $rules.RuleName -Recurse
+$results = Invoke-ScriptAnalyzer -Path "$($env:APPVEYOR_BUILD_FOLDER)" -IncludeRule $rules.RuleName -Recurse
 $results
 
 if ($results.Count -gt 0) {
